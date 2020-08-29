c561a54437774e8f0df4e63619b0811607607ce2
Nazhim <nazhimkalam@gmail.com>
2020-08-29 18:19:54 +0530

    completed the Footer section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Home from './components/Home/Home';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Home />
+      <Footer />
     </div>
   );
 }
