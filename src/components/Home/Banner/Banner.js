e9e15fce05c66948e60f06bbdea48e46821a434f
Nazhim <nazhimkalam@gmail.com>
2020-08-29 14:38:56 +0530

    banner design completed and now working on the datepicker

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Banner.css';
+import { Button } from '@material-ui/core';
 
 function Banner() {
+	const [showSearch, setShowSearch] = useState(false);
 	return (
 		<div className="banner">
+			<div className="banner__search"> 
+				<Button onClick = {() => setShowSearch(!showSearch)} variant="outlined" className="banner__searchButton">
+					Search Dates
+				</Button>
+			</div>
 			<div className="banner__info">
 				<h1>Get out and stretch your imagination</h1>
-                <h5>
-                    Plan a different kind of getaway to uncover the hidden gens near you.
-                </h5>
+				<h5>Plan a different kind of getaway to uncover the hidden gens near you.</h5>
+				<Button variant="outlined">Explore Nearby</Button>
 			</div>
 		</div>
 	);
