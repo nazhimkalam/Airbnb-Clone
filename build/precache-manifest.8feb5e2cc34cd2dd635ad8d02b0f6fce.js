714572bc41c4a58d324d155d2ebd66865deae6b6
Nazhim <nazhimkalam@gmail.com>
2020-08-29 22:22:03 +0530

    deployed to firebase and COMPLETED

---
+++ build/precache-manifest.8feb5e2cc34cd2dd635ad8d02b0f6fce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "180669e0cd6cfe020396a2767d81c019",
+    "url": "/index.html"
+  },
+  {
+    "revision": "333942ac138d0f9a7ddc",
+    "url": "/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "28e5014f24bfee03f8ff",
+    "url": "/static/css/main.bb7ded1e.chunk.css"
+  },
+  {
+    "revision": "333942ac138d0f9a7ddc",
+    "url": "/static/js/2.57c47215.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.57c47215.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28e5014f24bfee03f8ff",
+    "url": "/static/js/main.af7be3c5.chunk.js"
+  },
+  {
+    "revision": "31092b6647b1458628fe",
+    "url": "/static/js/runtime-main.f98f1a82.js"
+  }
+]);
