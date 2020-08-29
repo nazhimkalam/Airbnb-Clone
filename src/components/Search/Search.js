d9adbfe3868ce919aad963d222864855685e5907
Nazhim <nazhimkalam@gmail.com>
2020-08-29 15:55:05 +0530

    added the number of guest design

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,6 +3,8 @@ import './Search.css';
 import { DateRangePicker } from 'react-date-range';
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
+import PeopleIcon from '@material-ui/icons/People';
+import { Button } from '@material-ui/core';
 
 // working with the date picker component
 
@@ -23,6 +25,11 @@ function Search() {
 	return (
 		<div className="search">
 			<DateRangePicker ranges={[selectionRange]} onChange={handleSelect} />
+			<h2>
+				Number of guests <PeopleIcon />
+			</h2>
+			<input type="text" min={0} defaultValue={2} type="number" />
+			<Button>Search Airbnb</Button>
 		</div>
 	);
 }
