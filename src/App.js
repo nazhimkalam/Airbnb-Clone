771e914405376ac133fd29a499d1101d75075c06
Nazhim <nazhimkalam@gmail.com>
2020-08-29 18:42:17 +0530

    created the search page component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import './App.css';
 import Home from './components/Home/Home';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="app">
-      <Header />
-      <Home />
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="app">
+			<Router>
+				<Header />
+				<Switch>
+					<Route path = "/search">
+						<Home />
+					</Route>
+					<Route path = "/">
+						<SearchPage />
+					</Route>
+				</Switch>
+				<Footer />
+			</Router>
+		</div>
+	);
 }
 
 export default App;
