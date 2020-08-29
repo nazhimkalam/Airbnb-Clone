83f1e1eb1c8fff814d56c1144400966ad3880f28
Nazhim <nazhimkalam@gmail.com>
2020-08-29 13:08:15 +0530

    header component completed

--- src/components/Home/Header/Header.js
+++ src/components/Home/Header/Header.js
@@ -1,13 +1,32 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
+import LanguageIcon from '@material-ui/icons/Language';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Avatar } from '@material-ui/core';
 
 function Header() {
-    return (
-        <div className="header">
-            <img src="https://blog.stronglogos.com/wp-content/uploads/2014/08/airbnb_horizontal_lockup_web.png" className="header__icon" alt="" />
-        </div>
-    )
+	return (
+		<div className="header">
+			<img
+				src="https://blog.stronglogos.com/wp-content/uploads/2014/08/airbnb_horizontal_lockup_web.png"
+				className="header__icon"
+				alt=""
+			/>
+
+			<div className="header__center">
+				<input type="text" />
+				<SearchIcon />
+			</div>
+
+			<div className="header__right">
+				<p>Become a host</p>
+				<LanguageIcon />
+				<ExpandMoreIcon />
+				<Avatar />
+			</div>
+		</div>
+	);
 }
 
-export default Header
+export default Header;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,6 @@ import './Home.css'
 function Home() {
     return (
         <div className="home">
-            <h1>Home Component</h1>
         </div>
     )
 }
