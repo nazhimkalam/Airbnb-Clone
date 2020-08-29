428a494a36203bc085276dc74d0feea6efabc183
Nazhim <nazhimkalam@gmail.com>
2020-08-29 11:49:48 +0530

    created the home and it's components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="app">
-     
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+    return (
+        <div className="banner">
+            
+        </div>
+    )
+}
+
+export default Banner
